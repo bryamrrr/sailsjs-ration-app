1b07c85d869c63db6a683216fb4baabfc8e69ef5
Bryam Rodriguez <brodriguezaybar@gmail.com>
2018-04-16 10:35:38 -0500

    Create Thing model

--- api/controllers/things/view-available-things.js
+++ api/controllers/things/view-available-things.js
@@ -17,10 +17,13 @@ module.exports = {
 
 
   fn: async function (inputs, exits) {
-    const things = [
-      { id: 1, label: 'Sweet Red Drill' },
-      { id: 2, label: 'Rad Mountain Bike' },
-    ];
+    // const things = [
+    //   { id: 1, label: 'Sweet Red Drill' },
+    //   { id: 2, label: 'Rad Mountain Bike' },
+    // ];
+
+    // TODO: come back to this and only fetch things that the current user is allowed to see
+    const things = await Thing.find();
 
     // Respond with view.
     return exits.success({
